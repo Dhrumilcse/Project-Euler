3e27aa6bb3acde4a2089b0582f0dcf9d39cb447e
Dhrumil Patel <dhrumilcse14@gmail.com>
2019-01-18 12:43:38 +0530

    Sum of Even Fibonacci Numbers

---
+++ Problem2.py
@@ -0,0 +1,20 @@
+#Even Fibonacci Numbers
+
+#Recursive function to find Fibonacci of the number
+def fib(n):
+    if (n == 0 or n == 1):
+        return(1)
+    else:
+        return(fib(n-1) + fib(n-2))
+
+#Printing out Sum of even Fibonacci numbers
+def testFib(n):
+    sum = 0
+    for i in range(1,n):
+        #print(fib(i))
+        if (fib(i) % 2 == 0):
+            sum = sum + fib(i)
+    print(sum)
+
+#Not exceeding 4 million
+testFib(33)
