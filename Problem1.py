672383758d55d4ffc54722d29a97acfbd9b4fa72
Dhrumil Patel <dhrumilcse14@gmail.com>
2019-01-18 11:32:38 +0530

    Multiples of 3 and 5 below 1000

--- Problem1.py
+++ Problem1.py
@@ -1,8 +1,8 @@
 #Multiples of 3 and 5 below 1000
 sum = 0
-for i in range (1,1000):
-    if (i % 3 == 0):
+for number in range (1,1000):
+    if (number % 3 == 0):
         sum = sum + i
-    elif (i % 5 == 0):
+    elif (number % 5 == 0):
         sum = sum + i
 print(sum)
